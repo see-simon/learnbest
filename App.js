b7b360cc0671cbce2dedecaf9c917fda47e0c379
Your Name <you@example.com>
2023-05-23 13:13:14 +0200

    commit

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './components/Login';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// navigator
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Login from "./components/Login";
+import GetStared from "./components/GetStarted";
+import Register from "./components/Register";
+import Home from "./components/Home";
+
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-      <Login/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="GetStarted"  component={GetStared}/>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Register" component={Register}/>
+        <Stack.Screen name="Home" component={Home}/>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
