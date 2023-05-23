4c4ffdb89f1c5a40bfeb0bd4492d8e44c39bd454
Your Name <you@example.com>
2023-05-23 12:50:21 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login';
+
+export default function App() {
+  return (
+      <Login/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
